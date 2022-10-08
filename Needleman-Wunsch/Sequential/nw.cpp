04500f1dddb13a9142894b1e0f4fcc1551fe340a
aramondeveloper <aramonh@unal.edu.co>
2022-10-08 16:15:18 -0500

    sequential algorithm

---
+++ Needleman-Wunsch/Sequential/nw.cpp
@@ -0,0 +1,137 @@
+/*
+  Needleman-Wunsch algorithm  
+  by Adriano Ramón Hernández
+  Universidad Nacional de Colombia
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+
+#define MAX_N 1001
+
+using namespace std;
+
+int n, m;
+int match_score, mismatch_score, gap_score;
+string A, B;
+int dp[MAX_N][MAX_N];
+
+/*
+ Needleman-Wunsch algorithm para determinar la alineación óptima entre dos secuencias
+ suponiendo una puntuación dada por matches , gaps and mismatches.
+ Complexity: O(n * m) time, O(n * m) memory
+*/
+
+void print_matrix()
+{
+    for(int loop = 0; loop < n; loop++){
+        for(int loop2 = 0; loop2 < m; loop2++){
+            printf("%d ", dp[loop][loop2]);
+        }
+    }
+}
+
+/* 
+Rellena la matriz segun las 2 secuncial iniciales, 
+Acorde a los score en matchs, mismatches y gaps
+*/
+inline int needleman_wunsch()
+{
+    for (int i=0;i<=n;i++) dp[i][0] = dp[0][i] = -i * gap_score;
+    for (int i=1;i<=n;i++)
+    {
+        for (int j=1;j<=m;j++)
+        {
+            int S = (A[i-1] == B[j-1]) ? match_score : -mismatch_score;
+            dp[i][j] = max(dp[i-1][j-1] + S, max(dp[i-1][j] - gap_score, dp[i][j-1] - gap_score));
+        }
+    }
+    /* print_matrix(); */
+    return dp[n][m];
+}
+
+/*
+Recorre la matriz generada,
+buscando el alineamiento mas optimo
+y generando las nuevas secuencias.
+*/
+inline pair<string, string> get_optimal_alignment()
+{
+    string retA, retB;
+    stack<char> SA, SB;
+    int ii = n, jj = m;
+    while (ii != 0 || jj != 0)
+    {
+        if (ii == 0)
+        {
+            SA.push('-');
+            SB.push(B[jj-1]);
+            jj--;
+        }
+        else if (jj == 0)
+        {
+            SA.push(A[ii-1]);
+            SB.push('-');
+            ii--;
+        }
+        else
+        {
+            int S = (A[ii-1] == B[jj-1]) ? match_score : -mismatch_score;
+            if (dp[ii][jj] == dp[ii-1][jj-1] + S)
+            {
+                SA.push(A[ii-1]);
+                SB.push(B[jj-1]);
+                ii--; jj--;
+            }
+            else if (dp[ii-1][jj] > dp[ii][jj-1])
+            {
+                SA.push(A[ii-1]);
+                SB.push('-');
+                ii--;
+            }
+            else
+            {
+                SA.push('-');
+                SB.push(B[jj-1]);
+                jj--;
+            }
+        }
+    }
+    while (!SA.empty())
+    {
+        retA += SA.top();
+        retB += SB.top();
+        SA.pop();
+        SB.pop();
+    }
+    return make_pair(retA, retB);
+}
+
+int main()
+{
+    match_score = 1, mismatch_score = 1, gap_score = 1; // Constants de score quemadas
+
+    A = "AGGGCT";
+    B = "AGGCA";
+	n = A.length(); // length of gene1
+	m = B.length(); // length of gene2
+	
+    needleman_wunsch();
+    pair<string, string> alignment = get_optimal_alignment();
+
+    printf("\n %s \n %s \n", alignment.first.c_str(), alignment.second.c_str());
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Needleman-Wunsch-Algorithm_git
-Needleman-Wunsch-Algorithm 
+Needleman-Wunsch-Algorithm - Adriano Ramón Hernández
+
+# Sequential
+Para ejecutar el algoritmo de forma secuencial.
+
+Dentro de la carpeta "Sequential" ejecuta los siguientes comandos:
+
+- Comandos:
+
+1. " g++ nw.cpp -o app "
+2. " time ./app "
